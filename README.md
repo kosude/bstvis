c3fdadfec5393ee14ebb669b61188ea494a80221
Jack <jackbennett0708@gmail.com>
2022-11-10 18:11:15 +0000

    Set up CMake project heirarchy
    Amendments:
      - 10/11/2022: added school project notice to the README

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # BSTvis
 
-Binary search tree library and visualiser
+Binary search tree library and visualiser. (School project)
---
+++ include/bstvis/testfn.h
@@ -0,0 +1,24 @@
+// -----------------------------------------------------------------------------
+// Copyright (c) 2022 Jack Bennett
+// -----------------------------------------------------------------------------
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY  KIND,  EXPRESS  OR
+// IMPLIED, INCLUDING BUT NOT LIMITED  TO  THE  WARRANTIES  OF  MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT  SHALL  THE
+// AUTHORS OR COPYRIGHT HOLDERS BE  LIABLE  FOR  ANY  CLAIM,  DAMAGES  OR  OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// -----------------------------------------------------------------------------
+
+#ifndef __BSTVIS_TESTFN_H
+#define __BSTVIS_TESTFN_H
+
+/**
+ * @brief Test function
+ *
+ * @param str String parameter
+ *
+ */
+void bvTestFunction(const char *str);
+
+#endif // !__BSTVIS_TESTFN_H
